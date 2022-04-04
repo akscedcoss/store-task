4515097c0126122378ab8b2ee093bae945b4af47
cedcoss <amnakumarsrivastava@cedcoss.com>
2022-04-04 18:57:30 +0530

    acl implemented and grated permsiions to accountant

--- src/app/Listener/NotificationsListener.php
+++ src/app/Listener/NotificationsListener.php
@@ -1,12 +1,13 @@
 <?php
 
 namespace App\Listener;
+
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
 
 use Phalcon\Events\Event;
 use Phalcon\Di\Injectable;
-
+use Permissions;
 /**
  * Undocumented class
  */
@@ -27,26 +28,59 @@ class NotificationsListener extends Injectable
         $value = json_decode($value);
         // Decoding JWT Token 
         $key = "cedcossKey";
-        try{
+        try {
           $decoded = JWT::decode($value->token, new Key($key, 'HS256'));
-          $role=$decoded->role_type;
+          $role = $decoded->role_type;
           echo $role;
           // die;
-          // Implement ACL here //////////////////////////
-        }
-        catch(\Exception $e)
-        {
-        echo $e->getMessage();
-        die;
-        }
+          // Acl Code  Starts here 
+          $aclfile = APP_PATH . '/security/acl.cache';
+          if (is_file($aclfile) == true) {
+    
+            $acl = unserialize(
+                file_get_contents($aclfile)
+            ); 
+            if (!$role || true !== $acl->isAllowed($role,   $controller, $action)) {
+              echo " </h1> Access denied 404 </h1><br>" ; 
+              echo "You are ".$role."  And Only Allowed For ";
+              $permissons = new Permissions();
+              $permissons = Permissions::find(['role_type'=>$role]);
+          
+              foreach ($permissons as $key=>$value)
+              {   echo "<br>";
+                echo $this->tag->linkTo(
+                  [
+                    strtolower($value->component) ."/".$value->action,
+                    $value->component."+".$value->action,
+                
+                   ] );
+                   echo "<br>";
 
+
+              }
+
+
+              die();
+            } 
+          }
+          else{
+            echo "No acl File";
+            die;
+          }
+
+          // Implement ACL here //////////////////////////
+        } catch (\Exception $e) {
+          echo $e->getMessage();
+          die;
+        }
       } else {
         // We are on Login Page  Go  with The Flow 
       }
     } else {
       // When Cookie is not Set   Redirect to Login 
+      // echo "Enter Berare";
+      // die;
       return $this->response->redirect('/login');
     }
-   
   }
 }
